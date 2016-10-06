1d1b7cabeac2d40beaf5845f69d6e01ec2d08ebb
Clément Hampaï <cypress@cypressxt.net>
2016-10-06 11:46:57 +0200

    update assets path

--- config/environments/preprod.rb
+++ config/environments/preprod.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
     config.active_record.dump_schema_after_migration = false
 
     config.action_mailer.default_url_options = { :host => 'ggc.cypressxt.net' }
-    config.action_controller.asset_host = "ggc.cypressxt.net"
 
     # ActionMailer Config
     # Setup for production - deliveries, no errors raised
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
     config.active_record.dump_schema_after_migration = false
 
     config.action_mailer.default_url_options = { :host => 'intranet.ggc2016' }
-    config.action_controller.asset_host = "intranet.ggc2016"
 
 
     config.action_mailer.delivery_method = :smtp
